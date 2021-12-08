3b34cd5bcbea0c11d9b169531f3e6ec8444defe5
indrajeet yadav <dev.indrajeetydv@gmail.com>
2021-12-08 22:57:36 +0530

    indrajeet portofolio

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {faCopyright} from '@fortawesome/free-solid-svg-icons';
 import {faFacebook} from '@fortawesome/free-brands-svg-icons';
 import {faInstagram} from '@fortawesome/free-brands-svg-icons';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -14,7 +14,7 @@ export class FooterComponent implements OnInit {
   copyright=faCopyright;
   facebook=faFacebook;
   twitter=faTwitter;
-  github=faGithub;
+  github=faLinkedin;
   instagram=faInstagram;
   constructor() { }
 
